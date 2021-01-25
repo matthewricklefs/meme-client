d8b7de7cc42f2a9cbd2d5e0bcd17404a025e20f9
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-25 13:41:24 -0500

    initial commit

---
+++ src/components/meme.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function meme() {
+  return (
+    <div>
+      <h1>Hello from Meme</h1>
+    </div>
+  );
+}
---
+++ src/components/memeForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function MemeForm(props) {
+  render();
+  return (
+    <div>
+      <h1>Hello from MemeForm</h1>
+    </div>
+  );
+}
+
+export default MemeForm;
