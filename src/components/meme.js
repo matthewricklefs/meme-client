c7398d5180a4cfa301e3d1cf70619fc2a646887a
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-25 14:32:59 -0500

    adds favorite image, adds basic meme card styles

--- src/components/meme.js
+++ src/components/meme.js
@@ -3,7 +3,20 @@ import React from "react";
 const Meme = (props) => {
   return (
     <div className="meme">
-      <h1>{props.text}</h1>
+      <div className="img-wrapper">
+        <img className="meme-img" src={props.image} alt="funny_meme" />
+      </div>
+      <p>{props.text}</p>
+
+      {props.favorite ? (
+        <img
+          src="https://library.kissclipart.com/20180830/fuw/kissclipart-twinkle-little-star-clip-art-clipart-twinkle-twin-0d72b7a5dc286d1e.jpg"
+          alt="star"
+        />
+      ) : null}
+
+      <button>Delete</button>
+      <button>Edit</button>
     </div>
   );
 };
